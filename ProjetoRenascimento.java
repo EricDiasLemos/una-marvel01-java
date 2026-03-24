0ff74e854c5537ff2a172903fea7ff5aad154a0a
EricDiasLemos <ericdias0603@gmail.com>
2026-03-24 20:04:11 -0300

    Create ProjetoRenascimento.java

---
+++ ProjetoRenascimento.java
@@ -0,0 +1,14 @@
+public class ProjetoRenascimento {
+        public static void main(String[] args) {
+            boolean possuiCoragem = true; // Variável para indicar se o capitão america possui coragem
+            int saudeFisica = 40; // Variável para indicar a saúde física do capitão america
+
+            if (possuiCoragem && saudeFisica > 30) {
+                System.out.println("O Capitão América está pronto para enfrentar os desafios e salvar o mundo!");
+            }
+            
+            else {
+                System.out.println("O Capitão América precisa de mais coragem e saúde física para enfrentar os desafios.");
+            }
+            }
+        }
